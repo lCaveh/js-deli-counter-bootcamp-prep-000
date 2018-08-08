b6ebe48725cbb4cda464ecaa36a72dd18548c3b2
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-08 21:52:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function takeANumber(current,name) {
   return "Welcome, " + name + ". You are number " + (current +1) + "in line.";
 }
-function nowServing()
+function nowServing(katzDeliLine) {
+  
+}
